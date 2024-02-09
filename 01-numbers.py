5c88ab36fcd53ef94bfcc1fb11abdb92c3e0a241
Tobias Knuth <tknuth@users.noreply.github.com>
2024-02-09 18:51:12 +0100

    formatting

--- 01-numbers.py
+++ 01-numbers.py
@@ -3,44 +3,34 @@
 # %%
 2 + 2
 
-
 # %%
 50 - 5 * 6
-
 
 # %%
 (50 - 5 * 6) / 4
 
-
 # %%
 8 / 5  # floating point number
-
 
 # %%
 17 // 3  # floor division
 
-
 # %%
 17 % 3  # modulus/remainder
 
-
 # %%
 5**2
-
 
 # %%
 import math
 
 math.inf > 3
 
-
 # %%
 math.inf * 0
 
-
 # %%
 math.sqrt(2)
-
 
 # %%
 # beware of floating point precision
