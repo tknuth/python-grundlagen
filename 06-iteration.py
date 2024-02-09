d13bf9dc6d56958d33f0363476df34436a34d9ed
Tobias Knuth <tknuth@users.noreply.github.com>
2024-02-09 19:28:20 +0100

    updated iteration examples

--- 06-iteration.py
+++ 06-iteration.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
 # %%
+languages = ["C", "C++", "Python", "Java", "Ruby", "JavaScript"]
+
+for language in languages:
+    print(language)
+
+# %%
+for language in languages:
+    print(language)
+    if language == "Python":
+        print("Python gefunden!")
+        break
+
+# %%
+for i, language in enumerate(languages):
+    print(f"{i+1}. {language}")
+
+# %%
 for i in range(10):
     # Gib eine Zahl aus, wenn sie gerade ist
     if i % 2 == 0:
@@ -15,3 +32,12 @@ while i < 10:
         continue
     print(i)
 
+# %%
+inventory = {"Apfel": 2, "Banane": 3}
+
+for k in inventory:
+    print(f"{k}: {inventory[k]}")
+
+# %%
+for k, v in inventory.items():
+    print(f"{k}: {v}")
