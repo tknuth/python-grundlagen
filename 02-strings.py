4586769fb4f2578f7fa5ff149fa19452bad260c8
Tobias Knuth <tknuth@users.noreply.github.com>
2024-02-09 18:49:28 +0100

    new string examples, formatting

--- 02-strings.py
+++ 02-strings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 # %%
-"Das Wort \"Wort\" ist ein Substantiv"
+'Das Wort "Wort" ist ein Substantiv'
 
 
 # %%
@@ -14,6 +14,14 @@ name = "Anna"
 
 
 # %%
+long_text = (
+    "Dies ist ein langer Text, "
+    "der über mehrere Zeilen geht. "
+    "Das ist praktisch. "
+    "Und so weiter."
+)
+
+# %%
 greeting + ", " + name
 
 
@@ -22,5 +30,27 @@ f"{greeting}, {name}"
 
 
 # %%
+new_greeting = greeting.replace("Morgen", "Abend")
+
+# %%
+help(str.replace)
+
+
+# %%
 3 * "ab" + "c"
 
+# %%
+", ".join(["a", "b", "c"])
+
+# %%
+"Dies ist ein Satz, der auch ein Komma enthält.".split()
+
+# %%
+print("Alle drei Wörter\n folgt eine neue\n Zeile.")
+
+# %%
+message = " HiER ist eTwAS schieFgegangen."
+message.strip().lower().capitalize().replace("etwas", "nichts")
+
+# %%
+f"Die Grußformeln sind {greeting=} und {new_greeting=}."
