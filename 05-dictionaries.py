97865c31e805f7568a29e240ba9ed5cf033e6f6f
Tobias Knuth <tknuth@users.noreply.github.com>
2024-02-09 19:17:58 +0100

    updated dictionary examples

--- 05-dictionaries.py
+++ 05-dictionaries.py
@@ -1,16 +1,61 @@
 #!/usr/bin/env python
 # coding: utf-8
 # %%
-inventory = {"apple": 2, "banana": 3}
-
+inventory = {"Apfel": 2, "Banane": 3}
 
 # %%
-inventory["pineapple"] = 1
+inventory["Apfel"] = 1
 
+# %%
+inventory["Erdbeere"] = 5
+
+# %%
+del inventory["Banane"]
+
+# %%
+inventory["Ananas"] = 1
 
 # %%
 sorted(inventory)
 
+# %%
+values = inventory.values()
+
+# %%
+sum(values)
+
+# %%
+inventory["Erdbeere"] = 4
+sum(values)
+
+# %%
+inventory["Pfirsich"]
+
+# %%
+inventory.get("Pfirsich")
+
+# %%
+inventory.get("Pfirsich", 0)
+
+# %%
+inventory.pop("Apfel")
+
+# %%
+inventory.pop("Pfirsich")
+
+# %%
+print(inventory.pop("Pfirsich", 0))
+inventory
+
+# %%
+print(inventory.setdefault("Heidelbeere", 0))
+inventory
+
+# %%
+x = {"a": 1, "b": 2}
+y = {"a": 3, "b": 4}
+
+x == y
 
 # %%
 x = {"a": 1, "b": 2}
@@ -18,7 +63,6 @@ y = {"c": 3}
 
 x.update(y)
 x
-
 
 # %%
 x = {"a": 1, "b": 2}
@@ -28,3 +72,9 @@ print({**x, **y})
 print(x | y)
 print(1 | 2)
 
+
+# %%
+{["a", "b"]: 1}
+
+# %%
+{("a", "b"): 1}
