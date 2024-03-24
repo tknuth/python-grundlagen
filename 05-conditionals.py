60d3f9514252f50078a99502f859d2b0b5493a08
Tobias Knuth <tknuth@users.noreply.github.com>
2024-03-24 16:23:53 +0100

    added newlines

--- 05-conditionals.py
+++ 05-conditionals.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 # %%
 type(True)
+
 
 # %%
 print(f"{1 == 1}")
 print(f"{1 != 0}")
 print(f"{1 >= 0}")
 
+
 # %%
 # paarweise Vergleiche
 print(f"{2 >  1 > 0}")
 
+
 # %%
 "a" in ["a", "b", "c"]
+
 
 # %%
 weather = "Regen"
@@ -28,11 +31,13 @@ elif weather == "Schnee":
 else:
     print("Nimm nichts mit.")
 
+
 # %%
 a = 1
 b = 2
 
 3 if a > b else 4
+
 # %%
 # Truthy/Falsy und Short-Circuiting
 # https://docs.python.org/3/library/stdtypes.html#truth-value-testing
