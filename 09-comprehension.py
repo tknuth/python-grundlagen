006931a9e2539e6b13c50abdd907a8aaa65f2be6
Tobias Knuth <5556920+tknuth@users.noreply.github.com>
2024-03-24 16:26:21 +0100

    Update 09-comprehension.py

--- 09-comprehension.py
+++ 09-comprehension.py
@@ -5,12 +5,10 @@
 
 
 # %%
-inventory = {"Apfel": 2, "Banane": 3}
-
-
-# %%
 {v: len(v) for v in ["Apfel", "Banane", "Melone"]}
 
 
 # %%
+inventory = {"Apfel": 2, "Banane": 3}
+
 [f"{v}x {k}" for k, v in inventory.items()]
