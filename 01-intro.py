a6821aab55a968cd362825e2e5b54d04c84ee187
Tobias Knuth <tknuth@users.noreply.github.com>
2024-03-24 16:21:46 +0100

    added newlines

--- 01-intro.py
+++ 01-intro.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 # %%
 3 * 5 / 2  # Python als Taschenrechner
 
--- 02-numbers.py
+++ 02-numbers.py
@@ -3,40 +3,53 @@
 # %%
 1  # Ganzzahl
 
+
 # %%
 1.3  # Fließkommazahl
+
 
 # %%
 2 + 2
 
+
 # %%
 50 - 5 * 6
+
 
 # %%
 (50 - 5 * 6) / 4
 
+
 # %%
 8 / 5
+
 
 # %%
 17 // 3  # Ganzzahldivision
 
+
 # %%
+
 17 % 3  # Modulus/Rest
+
 
 # %%
 5**2
+
 
 # %%
 import math
 
 math.inf > 3
 
+
 # %%
 math.inf * 0
 
+
 # %%
 math.sqrt(2)
+
 
 # %%
 # Vorsicht, Fließkommazahlen können zu unerwarteten Ergebnissen führen
