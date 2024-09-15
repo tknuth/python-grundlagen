ac4e34f1b80c95eb1756900410f5013f73eaef51
Tobias Knuth <tknuth@users.noreply.github.com>
2024-09-15 19:07:21 +0200

    updated intro

--- 01-intro.py
+++ 01-intro.py
@@ -1,67 +1,88 @@
 #!/usr/bin/env python
 # coding: utf-8
 # %%
-3 * 5 / 2  # Python als Taschenrechner
-
-
-# %%
-s = "Hallo Welt!"  # Zuweisung von Variablen
-
+print("Willkommen im JupyterLab!")
 
 # %%
-s[0]  # Der Index beginnt bei 0.
-
-
-# %%
-s[3:5]  # Slicing bildet ein Intervall [a:b).
-
+# Ganzzahl
+1
 
 # %%
-s[3:]  # Beide Grenzen sind optional.
-
-
-# %%
-3 + 5  # Addition von Zahlen
-
+# Fließkommazahl
+1.3
 
 # %%
-"3" + "5"  # Konkatenation von Strings
-
-
-# %%
-3 * 5  # Multiplikation von Zahlen
-
+# Addition
+2 + 2
 
 # %%
-"3" * 5  # Multiplikation von Strings
-
-
-# %%
-str(3)  # explizite Konvertierung
-
+# Potenzierung
+5**2
 
 # %%
-l = [1, 2, 3]  # Liste
-l.append(4)  # Methode
-
-
-# %%
-# Iteration
-for i in l:
-    print(i)
-
+# Beachten Sie die Operatorpräzedenz nach PEMDAS.
+(50 - 5 * 6) / 4
 
 # %%
-for i in l:
-    # Bedingungen
-    if i % 2 == 0:
-        print(i)
-
+# Ganzzahldivision
+17 // 3
 
 # %%
-# Funktionsdefinition
-def f(x):
-    return x**2
+# Modulus/Rest
+17 % 3
 
+# %%
+import math
 
-f(3)  # Funktionsaufruf
+math.sqrt(2)
+
+# %%
+# Vorsicht, Fließkommazahlen können zu unerwarteten Ergebnissen führen.
+0.1 + 0.2 == 0.3
+
+# %%
+price = 19.99
+discount = 0.25
+round(price * (1 - discount), 2)
+
+# %%
+# Vertauschen Sie die Werte von a und b.
+a = 1
+b = 2
+
+# %%
+name = input("Wie heißt du?")
+"Hallo " + name
+
+# %%
+# Konkatenation von Strings
+"3" + "4"
+
+# %%
+# Vervielfachung von Strings
+"3" * 4
+
+# %%
+# Hilfe zu Objekten/Funktionen
+help(print)
+
+# %%
+# Die Funktion `type` gibt den Datentyp an.
+type(3.1)
+
+# %%
+# Was passiert hier?
+a = input("Nenne eine ganze Zahl für a:")
+b = input("Nenne eine ganze Zahl für b:")
+a + b
+
+# %%
+# Korrigieren Sie den Fehler.
+birth_year = input("In welchem Jahr wurdest du geboren?")
+age = 2024 - birth_year
+"Du bist " + age + " Jahre alt."
+
+# %%
+# Korrigieren Sie den Fehler.
+x = input("Gib eine ganze Zahl an:")
+"Das doppelte deiner Zahl lautet " + 2 * x
